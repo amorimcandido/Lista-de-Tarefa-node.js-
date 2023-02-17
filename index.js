78cdef6139dc6e0af1bcf6ba31e156ac3b460251
Mauricio Cândido <87139609+amorimcandido@users.noreply.github.com>
2023-02-17 11:11:00 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','html');
+app.use('/public',express.static(path.join(__dirname,'public')));
+app.set('views',path.join(__dirname,'/views'));
+
+var tarefas = ['Arrumar o quarto', 'Comprar no supermercado'];
+
+app.post('/',(req,res)=>{
+    tarefas.push(req.body.tarefa);
+    res.render('index',{tarefasList:tarefas});
+
+});
+
+app.get('/',(req,res)=>{
+
+    res.render('index',{tarefasList:tarefas});
+
+
+});
+
+app.get('/deletar/:id',(req,res)=>{
+    tarefas = tarefas.filter(function(val,index){
+        if(index != req.params.id){
+            return val;
+        };
+    });
+    res.render('index',{tarefasList:tarefas});
+});
+
+
+
+
+app.listen(8000,()=>{
+    console.log('Servidor está Rodando');
+
+});
+
